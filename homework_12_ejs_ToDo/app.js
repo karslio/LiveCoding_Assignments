206f1b96f89d4c6a734941bd01af96be420fdc5e
karslio <karslio@users.noreply.github.com>
2019-09-18 20:02:54 +0200

    updated

--- homework_12_ejs_ToDo/app.js
+++ homework_12_ejs_ToDo/app.js
@@ -1,40 +1,38 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
-
 const app = express();
-
+const port = 3333;
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 const items = ["Buy", "Cook", "Eat"];
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.redirect("/login");
 });
 
-app.get("/login", function(req, res) {
+app.get("/login", function (req, res) {
   const day = date.getDate();
   res.render("login", { listTitle: day });
 });
 
-app.post("/", function(req, res) {
+app.post("/login", function (req, res) {
   res.redirect("/todo");
 });
 
-app.get("/todo", function(req, res) {
-  const day = date.getDay();
+app.get("/todo", function (req, res) {
+  const day = date.getDate();
   res.render("todo", { listTitle: day, newListItems: items });
 });
 
-app.post("/", function(req, res) {
+app.post("/todo", function (req, res) {
   let item = req.body.newItem;
   items.push(item);
-  res.redirect("todo");
+  res.redirect("/todo");
 });
 
-const port = 3333;
-app.listen(port, function(req, res) {
+app.listen(port, function (req, res) {
   console.log(`Server started on port ${port}`);
 });
